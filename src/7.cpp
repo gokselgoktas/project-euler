008ca00f142123c155bbc2e2d9b1536e7f261928
Goksel Goktas <gokselgoktas@gmail.com>
2017-03-10 00:09:39 +0200

    Add 7

---
+++ src/7.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bitset>
+
+int main(int count, char *arguments[])
+{
+        std::bitset<1000000> sieve;
+        sieve.set();
+
+        for (unsigned i = 2; i * i < sieve.size(); ++i) {
+                if (sieve[i] == true) {
+                        for (unsigned k = i << 1; k < sieve.size(); k += i) {
+                                sieve[k] = false;
+                        }
+                }
+        }
+
+        unsigned r = 0;
+
+        for (unsigned i = 2; i < sieve.size(); ++i) {
+                if (sieve[i] == true) {
+                        if (++r == 10001) {
+                                std::cout << i << '\n';
+                                return 0;
+                        }
+                }
+        }
+
+        return 1;
+}
