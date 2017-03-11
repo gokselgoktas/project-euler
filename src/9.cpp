e95dc3a1df4e4d07e4fbd270b64b8d06404ba1d0
Goksel Goktas <gokselgoktas@gmail.com>
2017-03-11 23:50:13 +0200

    Add 9

---
+++ src/9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int count, char *arguments[])
+{
+        for (unsigned i = 1; i < 500; ++i) {
+                for (unsigned k = ((1000 - i) >> 1); k > 0; --k) {
+                        unsigned r = 1000 - (i + k);
+
+                        unsigned product = i * i + k * k - r * r;
+
+                        if (product == 0) {
+                                std::cout << i * k * r << '\n';
+                                return 0;
+                        }
+                }
+        }
+}
