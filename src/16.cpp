7f6e86d1d6f88844b1bb0f3c58da38d697d93a82
Goksel Goktas <gokselgoktas@gmail.com>
2017-04-04 16:28:34 +0200

    Add 16

---
+++ src/16.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+void add(std::string &result, std::string const &number)
+{
+        unsigned accumulator = 0;
+
+        auto i = result.rbegin();
+        for (auto k = number.rbegin(); k != number.rend(); ++k) {
+                accumulator += static_cast<unsigned>((*i - '0') + (*k - '0'));
+
+                *i = static_cast<char>(accumulator % 10) + '0';
+
+                accumulator /= 10;
+                ++i;
+        }
+
+        while (accumulator > 0) {
+                if (i != result.rend()) {
+                        accumulator += static_cast<unsigned>(*i - '0');
+                        *i = static_cast<char>(accumulator % 10) + '0';
+
+                        ++i;
+                } else {
+                        result.insert(0, 1, static_cast<char>(accumulator) + '0');
+                }
+
+                accumulator /= 10;
+        }
+}
+
+int main(int count, char *arguments[])
+{
+        std::string result = "2";
+
+        for (int i = 0; i < 999; ++i) {
+                add(result, result);
+        }
+
+        unsigned sum = 0;
+        for (auto i : result) {
+                sum += static_cast<unsigned>(i - '0');
+        }
+
+        std::cout << sum << '\n';
+        return 0;
+}
