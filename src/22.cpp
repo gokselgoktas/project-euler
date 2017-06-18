d8cf1f55be914372aa9400bcdc6fbefab5c1fbda
Goksel Goktas <gokselgoktas@gmail.com>
2017-06-18 03:37:14 +0300

    Add 22

---
+++ src/22.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include <cstdint>
+#include <fstream>
+#include <set>
+
+static uint64_t getScore(std::string const &string) noexcept
+{
+        uint64_t score = 0;
+
+        for (auto i : string) {
+                score += static_cast<uint64_t>(i - '@');
+        }
+
+        return score;
+}
+
+int main(int count, char *arguments[])
+{
+        if (count == 1) {
+                return 1;
+        }
+
+        std::ifstream input(arguments[1]);
+
+        if (input.fail() == true) {
+                return 1;
+        }
+
+        std::set<std::string> container;
+        std::istream_iterator<std::string> iterator(input);
+
+        std::copy(iterator, std::istream_iterator<std::string>(),
+                std::inserter(container, container.begin()));
+
+        uint64_t score = 0;
+        uint64_t position = 1;
+
+        for (auto i : container) {
+                score += getScore(i) * position;
+                ++position;
+        }
+
+        std::cout << score << '\n';
+        return 0;
+}
